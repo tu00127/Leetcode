68db123a989b45ac198b2f6bc2803f88ddd48b45
tu0127 <sharondududu@gmail.com>
2023-02-17 14:48:33 +0800

    2/17

--- 2.add-two-numbers.py
+++ 2.add-two-numbers.py
@@ -12,6 +12,23 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+        answer = ListNode()
+        node = answer
+        temp = 0
+
+        while l1 or l2 or temp > 0:
+            if l1:
+                temp += l1.val
+                l1 = l1.next
+            if l2:
+                temp += l2.val
+                l2 = l2.next
+            node.next = ListNode(temp % 10)
+            node = node.next
+            temp = temp // 10
+        return answer.next
+
         
 # @lc code=end
 
